4d6f4033072288aa0fcef682235874b856379fc4
gustrb <gustavoreisbauer@gmail.com>
2025-08-18 14:36:10 -0300

    feat: add discovery

--- main.go
+++ main.go
@@ -4,8 +4,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
+
+var views = map[string]struct{}{}
+
+func init() {
+	// Get all the files in the templates folder
+	files, err := os.ReadDir("templates")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		path := filepath.Join("templates", file.Name())
+		views[path] = struct{}{}
+	}
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
@@ -21,10 +37,19 @@ func main() {
 }
 
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	var view string
+	if r.URL.Path == "/" {
+		view = "index.html"
+	} else {
+		view = r.URL.Path + ".html"
+	}
+
 	lp := filepath.Join("templates", "layout.html")
-	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
-	if fp == "" {
-		fp = filepath.Join("templates", "index.html")
+	fp := filepath.Join("templates", filepath.Clean(view))
+
+	if _, ok := views[fp]; !ok {
+		http.NotFound(w, r)
+		return
 	}
 
 	tmpl, err := template.ParseFiles(lp, fp)
