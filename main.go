87123b764b278184d55a5f831714fd408f16db97
gustrb <gustavoreisbauer@gmail.com>
2025-08-18 14:19:31 -0300

    feat: change stuff around

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := struct {
-			Name string
-		}{
-			Name: "Gustavo",
-		}
-		tmpl.Execute(w, data)
-	})
+	http.HandleFunc("/", serveTemplate)
 
-	http.ListenAndServe(":8080", nil)
+	log.Print("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
